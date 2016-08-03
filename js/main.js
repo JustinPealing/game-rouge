bde8f46f07ab85a9015735db6d08821c307ddd2d
Justin Pealing <justin@pealing.me.uk>
2016-08-03 20:43:49 +0100

    Refactor draw code.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 var FONT = 32;
 
 // the ascii display, as a 2d array of characters
-var asciidisplay;
-
+var display;
 var level;
 
 // initialize phaser, call create() once done
@@ -14,28 +13,30 @@ function create() {
     level = new Level();
     level.init();
     
-    // initialize ascii display
-    asciidisplay = [];
-    for (var y = 0; y < ROWS; y++) {
-        var newRow = [];
-        asciidisplay.push(newRow);
-        for (var x = 0; x < COLS; x++)
-            newRow.push(initCell('', x, y));
-    }
-    drawMap();
+    initDisplay();
+    draw();
 }
 
-function initCell(chr, x, y) {
-    // add a single cell in a given position to the ascii display
+function initDisplay() {
+    display = [];
+    for (var y = 0; y < ROWS; y++) {
+        var newRow = [];
+        display.push(newRow);
+        for (var x = 0; x < COLS; x++)
+            newRow.push(createCell(x, y));
+    }
+}
+
+function createCell( x, y) {
     var style = {
         font: FONT + "px monospace",
         fill: "#A23B6C"
     };
-    return game.add.text(FONT * 0.6 * x, FONT * y, chr, style);
+    return game.add.text(FONT * 0.6 * x, FONT * y, '', style);
 }
 
-function drawMap() {
+function draw() {
     for (var y = 0; y < ROWS; y++)
         for (var x = 0; x < COLS; x++)
-            asciidisplay[y][x].text = level.map[y][x];
+            display[y][x].text = level.map[y][x];
 }
